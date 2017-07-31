c922bb5e8ebd0fe25ff24a0fc985ff4e0a6e1a48
demonic <ian910297@gmail.com>
2017-07-31 10:39:08 +0800

    Add Image
    + basic css

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
+var SectionLen = 595 + 20;// Length + Margin-bottom
 
+$(document).ready(function() {
+  $('.scroll-btn').click(function() {
+    var scroll = new Scroll(document.body);
+    var scrollIndex = $(this).attr('scroll-index');
+    scroll.to(0, SectionLen * scrollIndex).then(function() {
+
+    });
+  })
 });
